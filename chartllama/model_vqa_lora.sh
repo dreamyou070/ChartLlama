17d3560dd04a5c8178377dbee3b3c8d908cd2c1f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-04 23:42:43 +0900

    Update model_vqa_lora.sh

--- chartllama/model_vqa_lora.sh
+++ chartllama/model_vqa_lora.sh
@@ -2,10 +2,8 @@
 
 result_name="result"
 output_name="result"
-model-path="chartllama" #Lora Checkpoint
 
 python llava/eval/model_vqa_lora.py \
-  --model-path ${model-path} \
   --question-file llava/${result_name}/question.json \
   --image-folder ./playground/data/ \
   --answers-file ./playground/data/ans.jsonl \
