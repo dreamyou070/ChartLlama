da545763aa7b1e8e89b3851c48f24055e094b987
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-05 21:14:53 +0900

    Update train_mem.py

--- ChartLlama-code/llava/train/train_mem.py
+++ ChartLlama-code/llava/train/train_mem.py
@@ -2,6 +2,11 @@
 # Adopted from tatsu-lab@stanford_alpaca. Below is the original copyright:
 # Make it more memory efficient by monkey patching the LLaMA model with FlashAttn.
 # Need to call this before importing transformers.
+import os, sys
+present = os.path.dirname(os.path.abspath(__file__))
+parent = os.path.dirname(present)
+sys.path.append(present)
+sys.path.append(parent)
 from llava.train.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
 #replace_llama_attn_with_flash_attn()
 from llava.train.train import train
