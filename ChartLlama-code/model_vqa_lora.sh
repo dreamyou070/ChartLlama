bd8a58743c5f25b4549e322f5be2b686ebd4188a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-05 10:56:03 +0900

    pretraining model

---
+++ ChartLlama-code/model_vqa_lora.sh
@@ -0,0 +1,9 @@
+python llava/eval/model_vqa_lora.py \
+ --model-path /your_path_to/LLaVA/checkpoints/${output_name} \
+ --question-file /your_path_to/question.json \
+ --image-folder ./playground/data/ \
+ --answers-file ./playground/data/ans.jsonl \
+ --num-chunks $CHUNKS \
+ --chunk-idx $IDX \
+ --temperature 0 \
+ --conv-mode vicuna_v1 &
