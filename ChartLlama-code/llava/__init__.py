478e8991f1a4ddf34c5e89a5300e1213b97e7c69
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-05 21:24:08 +0900

    training

--- ChartLlama-code/llava/__init__.py
+++ ChartLlama-code/llava/__init__.py
@@ -1,1 +1,2 @@
 from .model import LlavaLlamaForCausalLM
+import llava
--- ChartLlama-code/llava/train/train_mem.py
+++ ChartLlama-code/llava/train/train_mem.py
@@ -3,13 +3,16 @@
 # Make it more memory efficient by monkey patching the LLaMA model with FlashAttn.
 # Need to call this before importing transformers.
 import os, sys
-present = os.path.dirname(os.path.abspath(__file__))
-parent = os.path.dirname(present)
-sys.path.append(present)
+dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(dir_path)
+parent = os.path.dirname(dir_path)
 sys.path.append(parent)
+super_parent = os.path.dirname(parent)
+sys.path.append(super_parent)
 from llava.train.llama_flash_attn_monkey_patch import replace_llama_attn_with_flash_attn
 #replace_llama_attn_with_flash_attn()
-from llava.train.train import train
+#from llava.train.train import train
 
-if __name__ == "__main__":
-    train()
+#if __name__ == "__main__":
+#    train()
+
