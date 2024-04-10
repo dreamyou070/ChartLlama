405a315e8b23f612dad4546534624267356bf8d6
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-10 23:54:21 +0900

    chceck once

--- generate_code.sh
+++ generate_code.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-pretrain_mm_mlp_adapter="/mnt/private_yucheng/huggingface_hub/llava-v1.5-mlp2x-336px-pretrain-vicuna-13b-v1.5/mm_projector.bin"
+# pretrain_mm_mlp_adapter="/mnt/private_yucheng/huggingface_hub/llava-v1.5-mlp2x-336px-pretrain-vicuna-13b-v1.5/mm_projector.bin"
+CHUNKS=1
+IDX=0
 
 CUDA_VISIBLE_DEVICES=1 \
   python generate_code.py \
   --model_base "lmsys/vicuna-13b-v1.5" \
   --model_path "listen2you002/ChartLlama-13b" \
   --vision_tower "openai/clip-vit-large-patch14-336/" \
-  --pretrain_mm_mlp_adapter ${pretrain_mm_mlp_adapter} \
   --mm_projector_type "mlp2x_gelu" \
   --question-file "./data/ChartLlama-Dataset/ours/box_chart_100examples_simplified_qa.json" \
   --image-folder "./data/ChartLlama-Dataset/ours" \
