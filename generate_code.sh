5809ea8e63c553167f8253f2a56c6f49b20139bd
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-11 00:06:01 +0900

    a

--- generate_code.sh
+++ generate_code.sh
@@ -4,18 +4,18 @@
 CHUNKS=1
 IDX=0
 
-question-file=r"./data/vistext/vistext_qa.json"
-image-folder=r"./data/vistext/sy"
-answers-file=r"./data/vistext/sy_answer.json"
+question_file=r"./data/vistext/vistext_qa.json"
+image_folder=r"./data/vistext/sy"
+answers_file=r"./data/vistext/sy_answer.json"
 vision_tower="openai/clip-vit-large-patch14-336"
 CUDA_VISIBLE_DEVICES=1 \
   python generate_code.py \
   --model_base "lmsys/vicuna-13b-v1.5" \
   --model_path "listen2you002/ChartLlama-13b" \
   --vision_tower "${vision_tower}" \
-  --question-file "./data/ChartLlama-Dataset/ours/box_chart_100examples_simplified_qa.json" \
-  --image-folder "./data/ChartLlama-Dataset/ours" \
-  --answers-file "./data/ChartLlama-Dataset/answer/box_chart_answer.json" \
+  --question_file "./data/ChartLlama-Dataset/ours/box_chart_100examples_simplified_qa.json" \
+  --image_folder "./data/ChartLlama-Dataset/ours" \
+  --answers_file "./data/ChartLlama-Dataset/answer/box_chart_answer.json" \
   --num-chunks $CHUNKS \
   --chunk-idx $IDX \
   --temperature 0 \
