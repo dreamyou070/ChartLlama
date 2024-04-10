11821fa950022e2eb4a9e8b4c163ee026f1027f1
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-10 23:51:35 +0900

    generate code test

--- generate_code.sh
+++ generate_code.sh
@@ -2,39 +2,17 @@
 
 pretrain_mm_mlp_adapter="/mnt/private_yucheng/huggingface_hub/llava-v1.5-mlp2x-336px-pretrain-vicuna-13b-v1.5/mm_projector.bin"
 
-deepspeed generate_code.py \
-  --deepspeed ./scripts/zero3.json \
-  --model-base "lmsys/llama2_models/vicuna-13b-v1.5" \
-  --model_path /mnt/gyfs/yuchenghan/llama2_models/vicuna-13b-v1.5 \
-  --lora_enable True \
-  --version "v1" \
+CUDA_VISIBLE_DEVICES=1 \
+  python generate_code.py \
+  --model_base "lmsys/vicuna-13b-v1.5" \
+  --model_path "listen2you002/ChartLlama-13b" \
   --vision_tower "openai/clip-vit-large-patch14-336/" \
   --pretrain_mm_mlp_adapter ${pretrain_mm_mlp_adapter} \
   --mm_projector_type "mlp2x_gelu" \
-  --mm_use_im_start_end False \
-  --mm_use_im_patch_token False \
-  --mm_vision_select_layer -2 \
   --question-file "./data/ChartLlama-Dataset/ours/box_chart_100examples_simplified_qa.json" \
   --image-folder "./data/ChartLlama-Dataset/ours" \
   --answers-file "./data/ChartLlama-Dataset/answer/box_chart_answer.json" \
-  --lora_r 64 \
-  --fp16 True \
-  --output_dir ./checkpoints/llava_mix_plus_chartqa \
-  --num_train_epochs 1 \
-  --per_device_train_batch_size 2 \
-  --per_device_eval_batch_size 4 \
-  --gradient_accumulation_steps 8 \
-  --evaluation_strategy "no" \
-  --save_strategy "steps" \
-  --save_steps 50000 \
-  --save_total_limit 1 \
-  --learning_rate 2e-5 \
-  --weight_decay 0. \
-  --warmup_ratio 0.03 \
-  --lr_scheduler_type "cosine" \
-  --logging_steps 1 \
-  --tf32 False \
-  --model_max_length 2048 \
-  --gradient_checkpointing True \
-  --lazy_preprocess True \
-  --dataloader_num_workers 4
+  --num-chunks $CHUNKS \
+  --chunk-idx $IDX \
+  --temperature 0 \
+  --conv-mode vicuna_v1
