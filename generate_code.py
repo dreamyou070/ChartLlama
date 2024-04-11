d97b64e506883715cea22ad3d3d6ab9d77acfcf9
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-11 10:00:50 +0900

    a

--- generate_code.py
+++ generate_code.py
@@ -306,28 +306,30 @@ def eval_model(args):
     for (input_ids, image_tensor), line in tqdm(zip(data_loader, questions), total=len(questions)):
         idx = line["id"]
         cur_prompt = line["conversations"][0]['value'].replace(DEFAULT_IMAGE_TOKEN, '').strip()
-
-        stop_str = conv_templates[args.conv_mode].sep if conv_templates[
-                                                             args.conv_mode].sep_style != SeparatorStyle.TWO else \
+        print(f'cur_prompt = {cur_prompt}')
+        stop_str = conv_templates[args.conv_mode].sep if conv_templates[args.conv_mode].sep_style != SeparatorStyle.TWO else \
+        print(f' stop_str = {stop_str}')
         conv_templates[args.conv_mode].sep2
+
         input_ids = input_ids.to(device=device, non_blocking=True)
+        print(f' input_ids = {input_ids}')
 
         with torch.inference_mode():
-            output_ids = model.generate(
-                input_ids,
-                images=image_tensor.to(dtype=torch.float16, device='cuda', non_blocking=True),
-                do_sample=True if args.temperature > 0 else False,
-                temperature=args.temperature,
-                top_p=args.top_p,
-                num_beams=args.num_beams,
-                max_new_tokens=1636,
-                use_cache=True)
-
+            output_ids = model.generate(input_ids,
+                                        images=image_tensor.to(dtype=torch.float16, device=device, non_blocking=True),
+                                        do_sample=True if args.temperature > 0 else False,
+                                        temperature=args.temperature,
+                                        top_p=args.top_p,
+                                        num_beams=args.num_beams,
+                                        max_new_tokens=1636,
+                                        use_cache=True)
+        print(f' model output = {output_ids}')
         input_token_len = input_ids.shape[1]
         n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
         if n_diff_input_output > 0:
             print(f'[Warning] {n_diff_input_output} output_ids are not the same as the input_ids')
         outputs = tokenizer.batch_decode(output_ids[:, input_token_len:], skip_special_tokens=True)[0]
+        print(f' batchwise decoding, outputs = {outputs}')
         outputs = outputs.strip()
         if outputs.endswith(stop_str):
             outputs = outputs[:-len(stop_str)]
