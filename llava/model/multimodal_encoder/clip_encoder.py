4d2235f92071372203fb7b51fb8d6513c0b729a9
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-11 11:56:55 +0900

    self modeling

--- llava/model/multimodal_encoder/clip_encoder.py
+++ llava/model/multimodal_encoder/clip_encoder.py
@@ -45,7 +45,11 @@ class CLIPVisionTower(nn.Module):
                 image_feature = self.feature_select(image_forward_out).to(image.dtype)
                 image_features.append(image_feature)
         else:
-            image_forward_outs = self.vision_tower(images.to(device=self.device, dtype=self.dtype), output_hidden_states=True)
+            print(f'self.dtype = {self.dtype}')
+            print(f'self.device = {self.device}')
+            image_forward_outs = self.vision_tower(images.to(device=self.device,
+                                                             dtype=self.dtype),
+                                                   output_hidden_states=True)
             image_features = self.feature_select(image_forward_outs).to(images.dtype)
 
         return image_features
