e331478340ff691310d069b06666d43696dcd549
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-04-11 20:06:13 +0900

    answering check

--- generate_code.py
+++ generate_code.py
@@ -307,19 +307,16 @@ def eval_model(args):
                                         num_beams=args.num_beams,
                                         max_new_tokens=1636,
                                         use_cache=True)
-
-        print(f' model output = {output_ids}')
         input_token_len = input_ids.shape[1]
         n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
         if n_diff_input_output > 0:
             print(f'[Warning] {n_diff_input_output} output_ids are not the same as the input_ids')
         outputs = tokenizer.batch_decode(output_ids[:, input_token_len:], skip_special_tokens=True)[0]
-        print(f' batchwise decoding, outputs = {outputs}')
         outputs = outputs.strip()
         if outputs.endswith(stop_str):
             outputs = outputs[:-len(stop_str)]
         outputs = outputs.strip()
-
+        print(f'answer = {outputs}')
         ans_id = shortuuid.uuid()
         ans_file.write(json.dumps({"question_id": idx,
                                    "prompt": cur_prompt,
